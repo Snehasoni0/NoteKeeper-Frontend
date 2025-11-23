2dc826135b4b41a040369b2785486ff1ae25269a
deepmali890 <deepmali590@gmail.com>
2025-11-23 21:47:00 +0530

    fix

--- src/context/ContextProvider.jsx
+++ src/context/ContextProvider.jsx
@@ -1,7 +1,7 @@
-import axios from "axios";
 import { useContext, useEffect } from "react";
 import { useState } from "react";
 import { createContext } from "react"
+import axiosInstance from "../../lib/axios";
 
 const authContext = createContext();
 function ContextProvider({ children }) {
@@ -10,7 +10,7 @@ function ContextProvider({ children }) {
   useEffect(() => {
     const verifyUser = async () => {
       try {
-        const res = await axios.get('http://localhost:8000/api/auth/verify',{
+        const res = await axiosInstance.get('/api/auth/verify',{
           headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`
           }
